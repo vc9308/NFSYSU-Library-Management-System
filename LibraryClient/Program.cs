a9bac1c3d00aeab130abe82831adc35e0bb5fc92
Victor Chen <vc93@icloud.com>
2016-04-08 11:54:57 +0800

    Connecting database

--- LibraryClient/Program.cs
+++ LibraryClient/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LibraryDAL;
+using System.Configuration;
 
 namespace LibraryClient
 {
@@ -14,9 +16,12 @@ namespace LibraryClient
         [STAThread]
         static void Main()
         {
+            string strCn = ConfigurationManager.AppSettings["connectionString"];
+            Connection cn = new Connection(strCn);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Welcome());
         }
     }
 }
---
+++ LibraryDAL/Connection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LibraryDAL
+{
+    public class Connection
+    {
+        public static SqlConnection cn = null;
+
+        public Connection(string connectionString)
+        {
+            cn = new SqlConnection(connectionString);
+            cn.Open();
+        }
+
+        public static void CloseConnection()
+        {
+            cn.Close();
+        }
+    }
+}
